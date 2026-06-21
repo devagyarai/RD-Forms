667dbcd3273d777506a1eab4baf1a50f6c4f5257
ReadyNest Intern <intern@readynest.com>
2026-06-21 22:42:06 +0530

    fix: append download link to DOM to fix silent failure in some browsers

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -63,7 +63,9 @@ export function downloadCSV(rows, filename = "export.csv") {
   const link = document.createElement("a");
   link.href = url;
   link.download = filename;
+  document.body.appendChild(link);
   link.click();
+  document.body.removeChild(link);
   URL.revokeObjectURL(url);
 }
 
